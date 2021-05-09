fb70e43ca8abe7a5e2faa1d4a0a5fdce534d4f77
Niko Wahyudi <wahyudiniko0@gmail.com>
2021-05-09 18:18:15 +0700

    first commit

---
+++ mail.py
@@ -0,0 +1,33 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+subject = "An intruder has been detected"
+to = "wahyudiniko0@gmail.com"
+UserName = "raspipengirim1@gmail.com"
+UserPassword = "ihfpxvhrrjzuizlt"
+
+
+def sendEmail(ImgFileName):
+    img_data = open(ImgFileName, 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = UserName
+    msg['To'] = to
+
+    text = MIMEText("Here is an attached image of the intruder")
+    msg.attach(text)
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+
+    s = smtplib.SMTP("smtp.gmail.com:587")
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(UserName, UserPassword)
+    s.sendmail(to, to, msg.as_string())
+    print("Sending mail")
+    s.quit()

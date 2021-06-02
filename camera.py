fbc6a027e659b920d620ed6345cca13b76bdca45
wahyudiniko <60518051+wahyudiniko@users.noreply.github.com>
2021-06-02 14:27:52 +0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
-#Modified by smartbuilds.io | niko wahyudi
-#Date: 27.06.20
+#Modified by smartbuilds.io | Niko Wahyudi
+#Date: 27.06.20 | 02.06.21
 #Desc: This scrtipt is running a face recongition of a live webcam stream. This is a modifed
 
 #code of the orginal Ageitgey (GitHub) face recognition demo to include multiple faces.
@@ -57,13 +57,13 @@ class VideoCamera(object):
         
         process_this_frame = True
         
-            # Resize frame of video to 1/4 size for faster face recognition processing
+        # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
         
-       # Only process every other frame of video to save time
+        # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
@@ -86,20 +86,21 @@ class VideoCamera(object):
                 #         print("error sending an email !")
                 
                 #print(face_encoding)
-                # print(matches)
+                #print(matches)
 
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = known_person[best_match_index]
-                    # found_intruder = False
+                    #found_intruder = False
                 else:
+                    print("\n")
                     print("INTRUDER DETECTED !!! \n")
                     cv2.imwrite('img.jpg', image)
                     sendEmail('img.jpg')
-                    print("successfully sending an email. \n")
+                    print("===============================")
 
-                # print(name)
+                #print(name)
                 #print(face_locations)
                 face_names.append(name)
 

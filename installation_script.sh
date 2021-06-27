295b133fc361c314bc159aa694d82e1ee9312c83
wahyudiniko <60518051+wahyudiniko@users.noreply.github.com>
2021-06-27 18:52:19 +0700

    Update installation_script.sh

--- installation_script.sh
+++ installation_script.sh
@@ -59,3 +59,16 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-4.5.2/modules \
 -D BUILD_EXAMPLES=ON ..
 
+make
+sudo make install
+sudo ldconfig
+
+ls /usr/local/lib/python3.7/site-packages/cv2/python-3.7/
+cd /usr/local/lib/python3.7/site-packages/cv2/python-3.7/
+sudo mv cv2.cpython-37m-arm-linux-gnueabihf.so cv2.so
+
+cd ~/.virtualenvs/cv/lib/python3.7/site-packages/
+ln -s /usr/local/lib/python3.7/site-packages/cv2/python-3.7/cv2.so cv2.so
+
+
+
